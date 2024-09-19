13ca9c65b3f164b40e50921adf71bdce2930c5d2
beni oren <benioren123beni@gmail.com>
2024-09-19 15:59:51 +0300

    i add function that update the team

--- models/team.py
+++ models/team.py
@@ -1,14 +1,16 @@
+from enum import unique
+
 from db import db
 
 class Team(db.Model):
     __tablename__ = 'teams'
     id = db.Column(db.Integer, primary_key=True)
     teamname = db.Column(db.String(50), nullable=False)
-    C = db.Column(db.String(50), nullable=True)
-    PF = db.Column(db.String(50), nullable=True)
-    SF = db.Column(db.String(50), nullable=True)
-    SG = db.Column(db.String(50), nullable=True)
-    PG = db.Column(db.String(50), nullable=True)
+    C = db.Column(db.String(50),unique=True, nullable=True)
+    PF = db.Column(db.String(50),unique=True, nullable=True)
+    SF = db.Column(db.String(50),unique=True, nullable=True)
+    SG = db.Column(db.String(50),unique=True, nullable=True)
+    PG = db.Column(db.String(50),unique=True, nullable=True)
 
 
     def to_dict(self):
--- routes/teams.py
+++ routes/teams.py
@@ -7,11 +7,12 @@ bp_teams = Blueprint('teams', __name__)
 
 
 
-
+#יצירת קבוצה של שחקנים
 @bp_teams.route('/', methods=['POST'])
 def create_team():
     data = request.get_json()
-    if 'teamname' not in data or 'playerId' not in data or len(data['playerId']) < 5:
+    min_players = 5
+    if 'teamname' not in data or 'playerId' not in data or len(data['playerId']) < min_players:
         return jsonify({'error': 'Missing required fields'}), 400
     teamname = data['teamname']
     players_id = data['playerId']
@@ -20,11 +21,39 @@ def create_team():
         this_player = Player.query.filter_by(playerId=player).first()
 
         dict_position[this_player.position] = player
-    new_team = Team(teamname=teamname, C=dict_position.get('C', None),
-                    PF=dict_position.get('PF', None),
-                    SF=dict_position.get('SF', None), SG=dict_position.get('SG', None),
-                    PG=dict_position.get('PG', None) )
+    if len(dict_position) < min_players:
+        return jsonify({'error': 'Missing required fields'}), 400
+    new_team = Team(teamname=teamname, C=dict_position['C'],
+                    PF=dict_position['PF'],
+                    SF=dict_position['SF'],
+                    SG=dict_position['SG'],
+                    PG=dict_position['PG'])
+
     db.session.add(new_team)
     db.session.commit()
 
     return jsonify(new_team.to_dict()), 201
+
+@bp_teams.route('/<int:team_id>', methods=['PUT'])
+def update_team(team_id):
+    team = Team.query.get_or_404(team_id)
+    print(team.to_dict())
+    data = request.get_json()
+    min_players = 5
+    players_id = data['playerId']
+    dict_position = {}
+    for player in players_id:
+        this_player = Player.query.filter_by(playerId=player).first()
+
+        dict_position[this_player.position] = player
+    if len(dict_position) < min_players:
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    team.teamname = data['teamname'] if 'teamname' in data else team.teamname
+    team.C=dict_position['C']
+    team.PF=dict_position['PF']
+    team.SF=dict_position['SF']
+    team.SG=dict_position['SG']
+    team.PG=dict_position['PG']
+    db.session.commit()
+    return jsonify(team.to_dict()), 200
