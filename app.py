d303fe3587fe0a0ded9dc512dde8ad3269ec23ac
beni oren <benioren123beni@gmail.com>
2024-09-19 11:03:43 +0300

    i gave all the players and i converted to json

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from db import db
+from routes.read_api import bp_reader
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///NBA.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+with app.app_context():
+    db.create_all()
+
+
+app.register_blueprint(bp_reader, url_prefix='/api/reader')
+
+
 
 
 if __name__ == '__main__':
---
+++ routes/read_api.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify,Blueprint
+import json
+from services.servic_to_load_json import save_to_json
+from requests import request
+app = Flask(__name__)
+bp_reader = Blueprint('read', __name__)
+
+
+
+
+
+def fetch_player_data(years):
+    player_data = []
+
+    for year in years:
+        url = f"http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataTotals/query?season={year}&pageSize=1000"
+        response = request('GET', url)
+
+        if response.status_code == 200:
+            data = response.json()
+            player_data.extend(data)
+        else:
+            print(f"Failed to fetch data for season {year}: {response.status_code}")
+    save_to_json(player_data, 'nba_players_data.json')
+    return player_data
+
+
+
+
+@bp_reader.route('/', methods=['GET'])
+def fetch_player_data():
+    years = [2022, 2023, 2024]
+    player_data = []
+
+    for year in years:
+        url = f"http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataTotals/query?season={year}&pageSize=1000"
+        response = request('GET', url)
+
+        if response.status_code == 200:
+            data = response.json()
+            player_data.extend(data)
+        else:
+            print(f"Failed to fetch data for season {year}: {response.status_code}")
+    save_to_json(player_data, 'nba_players_data.json')
+    return player_data
+
+
+
+
+
+
+# years = [2022, 2023, 2024]
+# all_player_data = fetch_player_data(years)
+
+
+
+
+
